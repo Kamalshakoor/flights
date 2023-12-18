040c4b4f2d554bc6d98101679f8a8df8ccc14992
kamal shakoor <kamalshakoor29022000.com>
2023-12-18 19:25:31 +0500

    Added react routing

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Airline = () => {
+  return (
+    <div>This is to show detail of individual Airline</div>
+  )
+}
+
+export default Airline
---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Airline from './Airline/Airline';
+import Airlines from './Airlines/Airlines';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Airlines />} />
+      <Route path="/airline/:slug" element={<Airline />} />
+    </Routes>
+  );
+};
+
+export default App;
