10adb027f408df2d6437c4c6173f599e3b0b6162
kamal shakoor <kamalshakoor29022000.com>
2023-12-30 20:06:06 +0500

    displayed review below the flight name on show page, without refreshing page

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,49 +1,58 @@
-import React,{useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import Header from './Header'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Header from './Header';
+import Review from './Review';
+import FeedbackForm from './FeedbackForm';
+
 const Airline = () => {
-  const [airline, setAirline] = useState({})
-  const [review, setreview] = useState({})
-  const [loaded, setloaded] = useState(false)
-  const { slug } = useParams(); 
+  const [airline, setAirline] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  const { slug } = useParams();
+
   useEffect(() => {
-    // const slug = props.match.params.slug
-    const url = `/api/v1/airlines/${slug}`
-    axios.get(url)
-    .then((resp) => {
-      console.log(resp),
-      setAirline(resp.data),
-      setloaded(true)
-    })
+    const url = `/api/v1/airlines/${slug}`;
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp);
+        setAirline(resp.data);
+        setLoaded(true);
+      })
       .catch((error) => {
         console.log(error);
       });
-  }, [slug])
+  }, [slug]);
 
+  const handleReviewSubmit = (newReview) => {
+    // Update the state with the new review
+    setAirline((prevAirline) => ({
+      ...prevAirline,
+      included: [...prevAirline.included, newReview],
+    }));
+  };
 
-  return <>
-    <div
-      className="container mt-5"
-    >
-      <div
-        className="row"
-      >
-        <div className="col-sm-8">
-          {
-            loaded &&  
-            <Header 
-            attributes={airline.data.attributes} 
-            reviews = {airline.included}
-            />
-          }
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-8">
+            <div class="row">
+              {loaded && (
+                <>
+                  <Header attributes={airline.data.attributes} reviews={airline.included} />
+                  <Review reviews={airline.included} />
+                </>
+              )}
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <FeedbackForm attributes={airline.data} onReviewSubmit={handleReviewSubmit} />
+          </div>
         </div>
-        <div className="col-sm-4">Column</div>
       </div>
-      
-    </div>
-    
-  </>
-}
+    </>
+  );
+};
 
-export default Airline
+export default Airline;
